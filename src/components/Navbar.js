4d5e0aac35d35de2f604c8467e4cc2a4c57cd62c
Wesley Rogers <wprogers86@gmail.com>
2022-02-03 00:41:22 -0500

    updated css nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,18 +13,18 @@ export default function Navbar({ currentPage, handlePageChange }) {
    return ( 
        <div style={header}>
            <h1 className=''>Wesley Rogers</h1>
-   <ul className="nav " >
+   <ul className="nav nav-tabs flex-row" >
         <li className="nav-item">
-            <a href='#about' onClick={() => handlePageChange('About')} className='navbar'>About</a>
+            <a href='#about' onClick={() => handlePageChange('About')} className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}>About</a>
         </li>
         <li className="nav-item">
-            <a href='#portfolio' onClick={() => handlePageChange('Portfolio')} className='navbar'>Portfolio</a>
+            <a href='#portfolio' onClick={() => handlePageChange('Portfolio')} className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}>Portfolio</a>
         </li>
         <li className="nav-item">
-            <a href='#contact' onClick={() => handlePageChange('Contact')} className='navbar'>Contact</a>
+            <a href='#contact' onClick={() => handlePageChange('Contact')} className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>Contact</a>
         </li>
         <li className="nav-item">
-            <a href="#resume" onClick={() => handlePageChange('Resume')} className='navbar'>Resume</a>
+            <a href="#resume" onClick={() => handlePageChange('Resume')} className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>Resume</a>
         </li>
     </ul>
     </div>
